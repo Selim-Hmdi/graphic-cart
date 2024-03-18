1fd65d0a50b62e11d56120c63733d458e9373964
Selim Hoummadi <selim_hoummadi@hotmail.com>
2024-03-18 20:59:23 +0100

    initial commit

---
+++ graphic-cart-backend/src/main/java/com/project/graphiccartbackend/GraphicCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.graphiccartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphicCartBackendApplication.class, args);
+	}
+
+}
---
+++ graphic-cart-backend/src/test/java/com/project/graphiccartbackend/GraphicCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.graphiccartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
